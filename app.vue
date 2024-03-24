d5510cab068f32f95293dbe6f52b1cba70c901f7
Sina Sedaghat <sina@Sinas-MacBook-Pro.local>
2024-03-25 00:43:17 +0330

    install and confige vuetify

--- app.vue
+++ app.vue
@@ -1,5 +1,50 @@
-<template>
+<!-- <template>
   <div>
     <NuxtWelcome />
   </div>
+</template> -->
+
+<script setup lang="ts">
+import { useTheme } from 'vuetify';
+const theme = useTheme()
+const changeTheme = () => {
+  theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
+}
+</script>
+
+<template>
+ <v-row align="center" justify="center">
+    <v-col>
+      <div class="d-flex justify-center ga-2 mt-7">
+        <v-chip @click="changeTheme">
+          Change Theme
+        </v-chip>
+    
+        <v-chip color="primary">
+          Primary
+        </v-chip>
+    
+        <v-chip color="secondary">
+          Secondary
+        </v-chip>
+    
+        <v-chip color="error">
+          Error
+        </v-chip>
+    
+        <v-chip color="info">
+          Info
+        </v-chip>
+    
+        <v-chip color="success">
+          Success
+        </v-chip>
+
+        <v-chip color="warning">
+          Warning
+        </v-chip>
+      </div>
+    </v-col>
+   </v-row>
 </template>
+
---
+++ plugins/vuetify.ts
@@ -0,0 +1,50 @@
+// import this after install `@mdi/font` package
+import '@mdi/font/css/materialdesignicons.css'
+
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import type { ThemeDefinition } from "vuetify";
+
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+const lightTheme: ThemeDefinition = {
+  dark: false,
+  colors: {
+    primary: "#d35400",
+    secondary: "#8e44ad",
+    background: "#ecf0f1",
+    error: "#c0392b",
+    info: "#2980b9",
+    success: "#27ae60",
+    warning: "#f1c40f",
+  },
+};
+
+const darkTheme: ThemeDefinition = {
+  dark: true,
+  colors: {
+    primary: "#8e44ad",
+    secondary: "#d35400",
+    background: "#2f3640",
+    error: "#27ae60",
+    info: "#f1c40f",
+    success: "#c0392b",
+    warning: "#2980b9",
+  },
+};
+
+export default defineNuxtPlugin((app) => {
+  const vuetify = createVuetify({
+    components,
+    directives,
+    theme: {
+      defaultTheme: "light",
+      themes: {
+        light: lightTheme,
+        dark: darkTheme,
+      },
+    },
+  })
+  app.vueApp.use(vuetify)
+})
