b65380292e66cca50f1e1e780243a6d38a508379
Sina Sedaghat <sina@Sinas-MacBook-Pro.local>
2024-04-19 18:25:57 +0330

    use .client suffix for define component

--- components/Comments.client.vue
+++ components/Comments.client.vue
@@ -1,3 +1,4 @@
 <template>
   this is Comments.client.vue component and client side render
 </template>
+<!-- https://github.com/nuxt/nuxt/issues/23939 -->
--- pages/aboute.vue
+++ pages/aboute.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
+    this is aboute view
     <!-- <Comments /> -->
-    <NuxtPage />
   </div>
 </template>
